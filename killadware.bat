48d3da9db9b32c1e24057b543328e0d6e551810b
Silverlink34 <brandongaliher7@gmail.com>
2014-05-22 16:58:37 -0400

    Create killadware.bat
    Creation

---
+++ killadware.bat
@@ -0,0 +1,8 @@
+@Echo off
+echo Disclaimer: I am not responsible for any damage this may cause to your system.
+echo Remember, you are running a batch file made by a person. 
+echo If you are suspicious of its actions, please inspect the file yourself.
+color 04
+echo Searching for known adware files/folders...
+color
+pause
