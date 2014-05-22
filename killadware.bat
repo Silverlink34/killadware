8a93ea35e410455e244aa910ac0e7a35fba39815
Silverlink34 <brandongaliher7@gmail.com>
2014-05-22 17:00:22 -0400

    Update killadware.bat

--- killadware.bat
+++ killadware.bat
@@ -2,6 +2,8 @@
 echo Disclaimer: I am not responsible for any damage this may cause to your system.
 echo Remember, you are running a batch file made by a person. 
 echo If you are suspicious of its actions, please inspect the file yourself.
+REM The following command is meant to substitute a "wait" or "sleep" command. 
+ping 1.1.1.1 -n 1 -w 3000 > nul
 color 04
 echo Searching for known adware files/folders...
 color
